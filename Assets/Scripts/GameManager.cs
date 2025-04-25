c5c129f6b2ce79df8ec252bbf98144641aa8a20b
Huzaaiifa <huzaifafarrukh9800@gmail.com>
2025-04-25 23:50:22 +0500

    final touches
    uhh final touches?

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,6 +6,6 @@ public class GameManager : MonoBehaviour
     public void SceneLoad(int index)
     {
         // Load the scene with the given name
-        UnityEngine.SceneManagement.SceneManager.LoadScene(name);
+        UnityEngine.SceneManagement.SceneManager.LoadScene(index);
     }
 }
